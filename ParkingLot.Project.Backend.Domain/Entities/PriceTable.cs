12b141cfc74912ea5dcb7c26a5e89b6ed0419543
Natan Luis <luisnataan556@gmail.com>
2023-07-12 18:58:47 -0300

    Develop: PriceTable entity created.

---
+++ ParkingLot.Project.Backend.Domain/Entities/PriceTable.cs
@@ -0,0 +1,16 @@
+﻿namespace ParkingLot.Project.Backend.Domain.Entities
+{
+    public class PriceTable : BaseEntity
+    {
+        public double Price { get; set; }
+        public double TotalPrice { get; set; }
+        public double ChargedPrice { get; set; }
+
+        public PriceTable(double price, double totalPrice, double chargedPrice)
+        {
+            Price = price;
+            TotalPrice = totalPrice;
+            ChargedPrice = chargedPrice;
+        }
+    }
+}
