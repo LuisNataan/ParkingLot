7103e243c0757222ec12f1135e09098c2512166e
Natan Luis <luisnataan556@gmail.com>
2023-07-12 18:58:05 -0300

    Develop: Vehicle entity created.

---
+++ ParkingLot.Project.Backend.Domain/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingLot.Project.Backend.Domain.Entities
+{
+    public class Vehicle : BaseEntity
+    {
+        public string Plate { get; set; }
+
+        public Vehicle(string plate)
+        {
+            this.Plate = plate;
+        }
+    }
+}
