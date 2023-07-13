ccd51379363e4f7774e4139864621f1d370963d7
Natan Luis <luisnataan556@gmail.com>
2023-07-13 18:43:52 -0300

    Develop: Creating Generic repository and vehicle repository.

---
+++ ParkingLot.Project.Backend.Infra/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingLot.Project.Backend.Domain.Entities;
+using ParkingLot.Project.Backend.Domain.Interfaces.GenericRepository;
+using ParkingLot.Project.Backend.Infra.Context;
+
+namespace ParkingLot.Project.Backend.Infra.Repositories.GenericRepository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected readonly MainContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(MainContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual async Task<TEntity> GetById(int id) => await _dbSet.FirstOrDefaultAsync(x => x.Id == id && !x.Deleted);
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<TEntity> Query() => _dbSet.AsNoTracking();
+    }
+}
---
+++ ParkingLot.Project.Backend.Infra/Repositories/VehicleRepository.cs
@@ -0,0 +1,24 @@
+﻿using ParkingLot.Project.Backend.Domain.Entities;
+using ParkingLot.Project.Backend.Domain.Interfaces;
+using ParkingLot.Project.Backend.Infra.Context;
+using ParkingLot.Project.Backend.Infra.Repositories.GenericRepository;
+
+namespace ParkingLot.Project.Backend.Infra.Repositories
+{
+    public class VehicleRepository : GenericRepository<Vehicle>, IVehicleRepository
+    {
+        public VehicleRepository(MainContext context) : base(context)
+        {
+        }
+
+        public Task<Vehicle> AddVehicle(string plate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vehicle> GetVehicleByPlate(string plate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
