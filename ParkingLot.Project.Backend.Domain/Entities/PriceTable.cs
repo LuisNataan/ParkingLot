7606f0caa039165eaba0d51a0bc319858dc3298d
Natan Luis <luisnataan556@gmail.com>
2023-07-14 23:19:56 -0300

    Develop: Adding new properties to PriceTable.

--- ParkingLot.Project.Backend.Domain/Entities/PriceTable.cs
+++ ParkingLot.Project.Backend.Domain/Entities/PriceTable.cs
@@ -5,12 +5,16 @@
         public double Price { get; set; }
         public double TotalPrice { get; set; }
         public double ChargedPrice { get; set; }
+        public DateTime EntryTime { get; set; }
+        public DateTime ExitTime { get; set; }
 
-        public PriceTable(double price, double totalPrice, double chargedPrice)
+        public PriceTable(double price, double totalPrice, double chargedPrice, DateTime entryTime, DateTime exitTime)
         {
             Price = price;
             TotalPrice = totalPrice;
             ChargedPrice = chargedPrice;
+            EntryTime = entryTime;
+            ExitTime = exitTime;
         }
     }
 }
