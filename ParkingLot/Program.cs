bb3ba9f1ead23acc6a7d687d6a953d69b0bfb931
Natan Luis <luisnataan556@gmail.com>
2023-07-16 15:25:33 -0300

    Develop: Configuring connection string and program file.

--- ParkingLot/Program.cs
+++ ParkingLot/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using ParkingLot.Project.Backend.Application.Services;
+using ParkingLot.Project.Backend.Infra.Context;
+using ParkingLot.Project.Backend.Infra.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<VehicleRepository>();
+builder.Services.AddScoped<PriceTableRepository>();
+builder.Services.AddScoped<ParkingLotService>();
+
+var connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+builder.Services.AddDbContext<MainContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
