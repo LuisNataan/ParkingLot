bdb66d3d6a338ac20b5551fbc1f61eb46d7c1a66
Natan Luis <luisnataan556@gmail.com>
2023-07-17 12:38:16 -0300

    Develop: Creating Controllers.

---
+++ ParkingLot/Controllers/ParkingLotController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingLot.Project.Backend.Application.Services;
+using ParkingLot.Project.Backend.Domain.Entities;
+
+namespace ParkingLot.Project.Backend.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[ParkingLot]")]
+    public class ParkingLotController : ControllerBase
+    {
+        private readonly ParkingLotService _parkingLotService;
+        private readonly VehicleService _vehicleService;
+
+        public ParkingLotController(ParkingLotService parkingLotService, VehicleService vehicleService)
+        {
+            this._parkingLotService = parkingLotService;
+            this._vehicleService = vehicleService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> InsertEntryTime([FromBody] Vehicle vehicle)
+        {
+            await _vehicleService.InsertVehicleEntry(vehicle);
+            return Ok(vehicle);
+        }
+
+        [HttpPut("{plate}")]
+        public async Task<ActionResult> InsterExitTime(string plate)
+        {
+            await _vehicleService.InsertVehicleExit(plate);
+            return Ok(plate);
+        }
+
+        [HttpGet("{plate}/charged-price")]
+        public async Task<ActionResult<decimal>> CalculateChargedPrice(string plate, [FromQuery] DateTime entryTime, [FromQuery] DateTime exitTime)
+        {
+            decimal chargedPrice = await _parkingLotService.CalculateChargedPrice(plate, entryTime, exitTime);
+            return Ok(chargedPrice);
+        }
+    }
+}
---
+++ ParkingLot/Controllers/PriceTableController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingLot.Project.Backend.Application.Services;
+using ParkingLot.Project.Backend.Domain.Entities;
+
+namespace ParkingLot.Project.Backend.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[PriceTable]")]
+    public class PriceTableController : ControllerBase
+    {
+        private readonly PriceTableService _priceTableService;
+
+        public PriceTableController(PriceTableService priceTableService)
+        {
+            this._priceTableService = priceTableService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PriceTable>>> GetPriceTables()
+        {
+            List<PriceTable> priceTables = await _priceTableService.GetPriceTables();
+            return Ok(priceTables);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PriceTable>> GetPriceTableByDate([FromQuery] DateTime entryTime)
+        {
+            PriceTable priceTable = await _priceTableService.GetPriceTableByDate(entryTime);
+
+            if (priceTable == null)
+            {
+                return NotFound();
+            }
+            return Ok(priceTable);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPriceTable([FromBody] PriceTable priceTable)
+        {
+            await _priceTableService.AddPriceTable(priceTable);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePriceTable(int id, [FromBody] PriceTable priceTable)
+        {
+            priceTable.Id = id;
+
+            await _priceTableService.UpdatePriceTable(priceTable);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePriceTable(int id)
+        {
+            await _priceTableService.DeletePriceTable(id);
+            return Ok();
+        }
+    }
+}
